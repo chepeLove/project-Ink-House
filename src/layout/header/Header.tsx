d9f79b43de1377ff143dbe5ede039ede7333b60b
chepeLove <ilya.chepelov@gmail.com>
2023-11-13 17:53:17 +0300

    refactoring ccs styles

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import style from './Header.module.css'
+import common from '../../common/Container.module.css'
 import logo from '../../assets/logo.svg'
 import basket from '../../assets/basket.svg'
 
 export const Header = () => {
     return (
         <header className={style.header}>
-            <div className = {style.container}>
+            <div className = {common.container}>
                 <div className={style.logoWrapper}>
                     <img className={style.logoImage} src={logo} alt={'logo'}/>
                     <span className={style.logoText}>Ink.House</span>
@@ -16,7 +17,8 @@ export const Header = () => {
                        <li className={style.menuItem}><a className={style.menuLink} href="#">Репродукции</a></li>
                        <li className={style.menuItem}><a className={style.menuLink} href="#">Новинки</a></li>
                        <li className={style.menuItem}><a className={style.menuLink} href="#">О нас</a></li>
-                       <li><a href="#"><img className={style.basketImage} src={basket} alt="basket"/></a></li>
+                       <button className={style.buttonBasket}><img className={style.basketImage} src={basket} alt="basket"/></button>
+
                    </ul>
                 </nav>
             </div>
