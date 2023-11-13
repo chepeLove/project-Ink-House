58e283b985ffba14d47acca00e7d06817576d813
chepeLove <ilya.chepelov@gmail.com>
2023-11-13 17:52:58 +0300

    refactoring ccs styles

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './Footer.module.css'
+import common from '../../common/Container.module.css'
 import logo from '../../assets/logo.svg'
 import linkedin from '../../assets/linkedIn.svg'
 import instagram from '../../assets/instagram.svg'
@@ -8,28 +9,28 @@ import youtube from '../../assets/youtube.svg'
 export const Footer = () => {
     return (
         <footer className={style.footer}>
-            <div className={style.container}>
+            <div className={common.container}>
                 <div className={style.logoNumberWrapper}>
                     <div className={style.logoWrapper}>
                         <img src={logo} alt="logo"/>
                         <span className={style.logoText}>Ink.House</span>
                     </div>
-                    <h3 className={style.footerTitle}>+7 (999) 543-54-54</h3>
+                    <a href="#" className={style.footerNumber}>+7 (999) 543-54-54</a>
                     <a href="#" className={style.text}><span>Мастерская</span></a>
                 </div>
                 <div className={style.navWrapper}>
-                    <div className={style.reproductionWrapper}>
+                    <div className={style.navInnerBox}>
                         <h3 className={style.footerTitle}>Репродукции</h3>
                         <a href="#" className={style.text}><span>Франция</span></a>
                         <a href="#" className={style.text}><span>Германия</span></a>
                         <a href="#" className={style.text}><span>Англия</span></a>
                     </div>
-                    <div className={style.newItemsWrapper}>
+                    <div className={style.navInnerBox}>
                         <h3 className={style.footerTitle}>Новинки</h3>
                         <a href="#" className={style.text}><span>2021</span></a>
                         <a href="#" className={style.text}><span>2020</span></a>
                     </div>
-                    <div className={style.aboutUsWrapper}>
+                    <div className={style.navInnerBox}>
                         <h3 className={style.footerTitle}>О нас</h3>
                         <a href="#" className={style.text}><span>Художники</span></a>
                         <a href="#" className={style.text}><span>Менеджеры</span></a>
